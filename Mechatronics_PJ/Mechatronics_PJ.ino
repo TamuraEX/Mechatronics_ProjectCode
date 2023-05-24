23890e1b1aa017c270e397f4b01103f37e8fef7b
TamuraEX <36571492+TamuraEX@users.noreply.github.com>
2023-05-24 20:40:44 +0900

    Add files via upload

--- Mechatronics_PJ/Mechatronics_PJ.ino
+++ Mechatronics_PJ/Mechatronics_PJ.ino
@@ -18,14 +18,14 @@ const int button_servo3_CCW = 7;
 
 
 // サーボモータの初期角
-const int initialAngle1 = 90;　//ちゃんと決定したほうがいい
-const int initialAngle2 = 90;
-const int initialAngle3 = 90;
-//const int initialAngle4 = 90;
+int initialAngle1 = 0;//ちゃんと決定したほうがいい
+int initialAngle2 = 0;
+int initialAngle3 = 0;
+int initialAngle4 = 0;
 
 // サーボモータの回転角度の範囲（たぶん制約がいる）
-const int minAngle = 0;
-const int maxAngle = 180;
+int minAngle = 0;
+int maxAngle = 180;
 
 // ホールセンサの値の範囲(使うとき用)
 //const int minSensorValue = 0;
@@ -40,6 +40,12 @@ Servo servo4;
 // 関数のプロトタイプ宣言
 void calculateInverseKinematics(int sensorValue1, int sensorValue2, int sensorValue3, int sensorValue4);
 int mapSensorValueToAngle(int sensorValue, int minSensorValue, int maxSensorValue, int minAngle, int maxAngle);
+int pmax = 2400;
+int pmin = 10;
+int S_angle1;
+int S_angle2;
+int S_angle3;
+int S_angle4;
 
 
 void setup() {
@@ -53,22 +59,55 @@ void setup() {
   //pinMode(servoPin4, OUTPUT);
 
   // サーボオブジェクトにピン番号を割り当て
-  servo1.attach(servoPin1);
-  servo2.attach(servoPin2);
-  servo3.attach(servoPin3);
-  //servo4.attach(servoPin4);
+  servo1.attach(servoPin1,pmin,pmax);
+  servo2.attach(servoPin2,pmin,pmax);
+  servo3.attach(servoPin3,pmin,pmax);
+  //servo4.attach(servoPin4,pmin,pmax);
 
   // サーボモータの初期角度を設定
   servo1.write(initialAngle1);
   servo2.write(initialAngle2);
   servo3.write(initialAngle3);
   //servo4.write(initialAngle4);
+
+  S_angle1 = initialAngle1;
+  S_angle2 = initialAngle2;
+  S_angle3 = initialAngle3;
+  S_angle4 = initialAngle4;
 }
 
-//ここまでやった1744
+
 void loop() {
-  // ホールセンサからの入力値を読み取る
-  int sensorValue1 = Read(hallSensorPin1);　//たぶんこの辺でノイズが乗る。フィルタがいるかもしれない（ソフト的に？ハード的に？）
+  //スイッチの動作
+  if(digitalRead(button_servo1_CW) == 0){
+    if(digitalRead(button_servo1_CCW) == 1){
+    S_angle1 += 1;}
+  }
+  if(digitalRead(button_servo1_CCW) == 0){
+    if(digitalRead(button_servo1_CW) == 1){
+    S_angle1 -= 1;}
+  }
+  if(digitalRead(button_servo2_CW) == 0){
+    if(digitalRead(button_servo2_CCW) == 1){
+    S_angle2 += 1;}
+  }
+  if(digitalRead(button_servo2_CCW) == 0){
+    if(digitalRead(button_servo2_CW) == 1){
+    S_angle2 -= 1;}
+  }
+  if(digitalRead(button_servo3_CW) == 0){
+    if(digitalRead(button_servo3_CCW) == 1){
+    S_angle3 += 1;}
+  }
+  if(digitalRead(button_servo3_CCW) == 0){
+    if(digitalRead(button_servo3_CW) == 1){
+    S_angle3 -= 1;}
+  }
+
+
+
+  /* ホールセンサからの入力値を読み取る
+  int sensorValue1 = analogead(hallSensorPin1);　//たぶんこの辺でノイズが乗る。フィルタがいるかもしれない（ソフト的に？ハード的に？）
   int sensorValue2 = analogRead(hallSensorPin2);
   int sensorValue3 = analogRead(hallSensorPin3);
   int sensorValue4 = analogRead(hallSensorPin4);
@@ -86,25 +125,27 @@ void calculateInverseKinematics(int sensorValue1, int sensorValue2, int sensorVa
   int angle2 = mapSensorValueToAngle(sensorValue2, minSensorValue, maxSensorValue, minAngle, maxAngle);
   int angle3 = mapSensorValueToAngle(sensorValue3, minSensorValue, maxSensorValue, minAngle, maxAngle);
   int angle4 = mapSensorValueToAngle(sensorValue4, minSensorValue, maxSensorValue, minAngle, maxAngle);
+*/
 
   // サーボモータに角度をセット
-  servo1.write(angle1);
-  servo2.write(angle2);
-  servo3.write(angle3);
-  servo4.write(angle4);
+  servo1.write(S_angle1);
+  servo2.write(S_angle2);
+  servo3.write(S_angle3);
+  servo4.write(S_angle4);
+  delay(70);
 
   // 関節角度をシリアルモニタに表示（デバッグ用）
   Serial.print("Joint Angles: ");
-  Serial.print(angle1);
+  Serial.print(S_angle1);
   Serial.print(", ");
-  Serial.print(angle2);
+  Serial.print(S_angle2);
   Serial.print(", ");
-  Serial.print(angle3);
+  Serial.print(S_angle3);
   Serial.print(", ");
-  Serial.println(angle4);
+  Serial.println(S_angle4);
 }
 
 int mapSensorValueToAngle(int sensorValue, int minSensorValue, int maxSensorValue, int minAngle, int maxAngle) {
   // ホールセンサの値を関節角度の範囲にマッピング
-  return map(sensorValue, minSensorValue, maxSensorValue, minAngle, maxAngle);
+  return map(sensorValue, minSensorValue, maxSensorValue, minAngle, maxAngle); //まだ使ってない
 }
