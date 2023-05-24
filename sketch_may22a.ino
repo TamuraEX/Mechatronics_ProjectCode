b42e32542b1da9578e8c3970363a68f1ed93d4fd
TamuraEX <36571492+TamuraEX@users.noreply.github.com>
2023-05-24 19:10:40 +0900

    Add files via upload

--- sketch_may22a.ino
+++ sketch_may22a.ino
@@ -1,20 +1,32 @@
+#include<Servo.h>
+
+Servo servo;
+
 void setup() {
   // put your setup code here, to run once:
   // サーボモータのピン番号
-   pinMode(10, OUTPUT);
+  servo.attach(10, 500, 2400);
+  pinMode(10, OUTPUT);
 
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  digitalWrite(10, HIGH);
-  delayMicroseconds(2000);
-  digitalWrite(10, LOW);
+  servo.write(0);
   delay(1000);
-  digitalWrite(10, HIGH);
-  delayMicroseconds(3000);
-  digitalWrite(10, LOW);
+  servo.write(90);
   delay(1000);
 }
+
+/*
+// put your main code here, to run repeatedly:
+  servo(10,500,2500);
+  digitalWrite(10, HIGH);
+  delayMicroseconds(5000);
+  digitalWrite(10, LOW);
+  delay(1000);
+  digitalWrite(10, HIGH);
+  delayMicroseconds(5000);
+  digitalWrite(10, LOW);
+  delay(1000);
+  */
